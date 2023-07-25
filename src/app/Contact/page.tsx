e49203506db8515c5b5bad86939f554be84be62d
kunal666-del <mukula672@gmail.com>
2023-07-26 00:03:31 +0530

    finally

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
 const page = () => {
   return (
-    <div className='text-center mx-12'><h2 className='my-12 text-2xl font-bold'>Contact us for Save It</h2>
-   <p> Welcome to Save It, if you want to contact us, then feel free to say anything about https://save-it.vercel.app. We'll appreciate your feedback.</p>
+    <div className="text-center mx-12"><h2 className="my-12 text-2xl font-bold">Contact us for Save It</h2>
+   <p> Welcome to Save It, if you want to contact us, then feel free to say anything about https://save-it.vercel.app. We"ll appreciate your feedback.</p>
     
     <p>You can contact us:</p>
     <ul>
@@ -14,7 +14,7 @@ const page = () => {
    <li>- If you want to give suggestions to improve Save It</li>
    <li>- If you face any error or any issues on our site.</li>
     </ul>
-   <p> Don't hesitate to mail us on: mukula672@gmail.com</p></div>
+   <p> Don"t hesitate to mail us on: mukula672@gmail.com</p></div>
   )
 }
 
